a8511c056b91300ae1d62a51e1b43b8849460d04
Erica Dee Fox <ericadeefox@gmail.com>
2017-06-02 15:48:34 -0400

    Populate address & location details independently of the thumbnails

--- app/webroot/js/script.js
+++ app/webroot/js/script.js
@@ -312,17 +312,19 @@ function setupEventAccordion() {
 				var event_id = toggler.data('eventId');
 				var collapse = toggler.next('.collapse');
 				var thumbnail = toggler.siblings('.tiny_thumbnails').children('a.thumbnail:first-child');
-				if (thumbnail.length > 0) {
-					if (collapse.is(':visible') && collapse.css('height') != '0px') {
-						thumbnail.fadeIn(150);
-						toggler.find('.address').slideUp(150);
-						toggler.find('.location_details').slideUp(150);
-					} else {
-						thumbnail.fadeOut(500);
-						toggler.find('.address').css('display', 'block');
-						toggler.find('.location_details').css('display', 'block');
-					}
-				}
+                if (collapse.is(':visible') && collapse.css('height') != '0px') {
+                    if (thumbnail.length > 0) {
+                        thumbnail.fadeIn(150);
+                    }
+                    toggler.find('.address').slideUp(150);
+                    toggler.find('.location_details').slideUp(150);
+                } else {
+                    if (thumbnail.length > 0) {
+                        thumbnail.fadeOut(500);
+                    }
+                    toggler.find('.address').css('display', 'block');
+                    toggler.find('.location_details').css('display', 'block');
+                }
 
 				var more_info_id = 'more_info_'+event_id;
 				if (muncieEventsFeed.xfbml_parsed.indexOf(more_info_id) == -1) {
