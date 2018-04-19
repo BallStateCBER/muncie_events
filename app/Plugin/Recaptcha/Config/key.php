fac6b140ed658988549bdabd731108a7a5e18d48
Graham Watson <graham@phantomwatson.com>
2018-04-18 22:52:23 -0400

    Remove old Recaptcha plugin

--- app/Plugin/Recaptcha/Config/key.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configured for MuncieEvents.com
-$config = array(
-	'Recaptcha' => array(
-		'Public'  => '6Lcg6tkSAAAAALkenFi1dIQ5B-4BVLJur5hYl-2J',
-		'Private' => '6Lcg6tkSAAAAAFCoVDGXB5R8eshJmmtV24ZPzknC'
-	)
-);
